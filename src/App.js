03c73bd764fc6645217c9dab3631aa9d88e92f1d
seojinnie <1210_jin@naver.com>
2023-09-20 05:32:14 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
-import logo from "./logo.svg";
+import { Component } from "react";
+import Customer from "./components/Customer";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management</h2>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "971210",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "박서진",
+    birthday: "971210",
+    gender: "남자",
+    job: "개발자",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "박유빈",
+    birthday: "971210",
+    gender: "남자",
+    job: "디자이너",
+  },
+];
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {customers.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
